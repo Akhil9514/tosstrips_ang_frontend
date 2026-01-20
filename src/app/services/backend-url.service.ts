610c2dfe028c04a8c8909c402ff8de32629a0557
Akhil9514 <55031786+Akhil9514@users.noreply.github.com>
2026-01-20 21:51:36 +0530

    backend-domain-generated

--- src/app/services/backend-url.service.ts
+++ src/app/services/backend-url.service.ts
@@ -7,7 +7,7 @@ export class BackendUrlService {
 
   // private mainUrl: string = 'http://127.0.0.1:8000/'
   // private mainUrl: string ='https://mybackend.app/'
-  private mainUrl: string = "http://34.228.153.200/"
+  private mainUrl: string = "https://thierback.in/"
 
   private tourntripUrl: string = this.mainUrl + 'tourntrips/'; // Django backend URL
   private bookingUrl: string = this.mainUrl + 'bookings/'; // Django backend URL
